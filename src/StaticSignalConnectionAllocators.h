5cdff4303ff7b5071d79c8ad8dc20c9fd3e01dd4
Brandon Dewitt <brandonsdewitt@gmail.com>
2014-05-10 21:35:34 -0600

    add gtest runner

--- src/StaticSignalConnectionAllocators.h
+++ src/StaticSignalConnectionAllocators.h
@@ -5,19 +5,13 @@
 #include "SignalDefinitions.h"
 
 namespace jl {
+  typedef Signal0<void> TDummySignal;
 
-typedef Signal0<void> TDummySignal;
+  template< unsigned _Size >
+    class StaticSignalConnectionAllocator : public StaticObjectPoolAllocator< TDummySignal::eAllocationSize, _Size > { };
 
-template< unsigned _Size >
-class StaticSignalConnectionAllocator : public StaticObjectPoolAllocator< TDummySignal::eAllocationSize, _Size >
-{
-};
-
-template< unsigned _Size >
-class StaticObserverConnectionAllocator : public StaticObjectPoolAllocator< SignalObserver::eAllocationSize, _Size >
-{
-};
-    
+  template< unsigned _Size >
+    class StaticObserverConnectionAllocator : public StaticObjectPoolAllocator< SignalObserver::eAllocationSize, _Size > { };
 } // namespace jl
 
 #endif // ! defined( _JL_STATIC_SIGNAL_CONNECTION_ALLOCATORS_H_ )
